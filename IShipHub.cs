9c3e6e17d088df4214cdc15bea37fef0763070b7
Dohyeong Kim <o2kim0225@naver.com>
2024-06-02 12:27:20 +0900

    add interface

---
+++ IShipHub.cs
@@ -0,0 +1,13 @@
+﻿using MagicOnion;
+using System.Threading.Tasks;
+
+namespace GameServer.Hubs
+{
+
+    public interface IShipHub : IStreamingHub<IShipHub, IShipHubReceiver>
+    {
+        Task JoinAsync(string roomName, string userName);
+        Task LeaveAsync();
+        Task SendMessageAsync(string userName, string message);
+    }
+}
---
+++ IShipHubReceiver.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Hubs
+{
+    public interface IShipHubReceiver
+    {
+        void OnJoin(string userName);
+
+        void OnLeave(string userName);
+    }
+}
